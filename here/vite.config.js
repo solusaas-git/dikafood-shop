4e61286694a77ea0215ae4f46073a7b95a51f3ce
anaselbtioui <152001337+solu-anas@users.noreply.github.com>
2025-02-10 22:06:42 +0100

    deploy config

--- here/vite.config.js
+++ here/vite.config.js
@@ -5,6 +5,20 @@ import svgr from '@svgr/rollup'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: '/',
+  build: {
+    outDir: 'build',
+    assetsDir: 'assets',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
   // css: {
   //   preprocessorOptions: {
   //     scss: {
